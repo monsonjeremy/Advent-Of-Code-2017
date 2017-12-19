56c1ea499be5d8039b43375a745a7cd1f03ecf26
Jeremy Monson <monson.jeremy@gmail.com>
2017-12-19 15:55:26 -0800

    even more readable day 9

--- day9/day9.js
+++ day9/day9.js
@@ -14,10 +14,7 @@ Array.prototype.peek = function() {
 }
 
 // Parse the input by line
-const INPUT = fs
-  .readFileSync('./day9/day9.txt')
-  .toString('utf-8')
-  .split('')
+const INPUT = fs.readFileSync('./day9/day9.txt').toString('utf-8')
 
 // Globals
 const STACK = []
@@ -25,24 +22,13 @@ let IS_GARBAGE = false
 let SUM_VAL = 0
 let GARBAGE_COUNT = 0
 
-for (let currIndex = 0; currIndex < INPUT.length; currIndex += 1) {
-  // If current character is an ignore character we can safely skip forward in the loop
-  if (INPUT[currIndex] === '!') currIndex += 1
-  else if (IS_GARBAGE)
-    // It's garbage, just inc garbage count
-    GARBAGE_COUNT += 1
-  else if (INPUT[currIndex] === '<' && !IS_GARBAGE)
-    // Opening garbage tag, flip garbage state
-    IS_GARBAGE = true
-  else if (INPUT[currIndex] === '>' && IS_GARBAGE)
-    // Closing garbage tag, flip garbage state
-    IS_GARBAGE = false
-  else if (INPUT[currIndex] === '{')
-    // Push onto the stack for opening group char
-    STACK.push(STACK.peek() + 1)
-  else if (INPUT[currIndex] === '}')
-    // Pop off the stack for closing group char and add to the sum
-    SUM_VAL += STACK.pop()
+for (let index = 0, char = INPUT[0]; index < INPUT.length; index += 1, char = INPUT[index]) {
+  if (char === '!') index += 1
+  else if (char === '>' && IS_GARBAGE) IS_GARBAGE = false
+  else if (IS_GARBAGE) GARBAGE_COUNT += 1
+  else if (char === '<' && !IS_GARBAGE) IS_GARBAGE = true
+  else if (char === '{') STACK.push(STACK.peek() + 1)
+  else if (char === '}') SUM_VAL += STACK.pop()
 }
 
 console.log(`Part 1 solution = ${SUM_VAL}`)
