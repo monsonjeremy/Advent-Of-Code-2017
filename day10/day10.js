2f5a1a73be0d10007cd752a0dce1ccebfc3c22b1
Jeremy Monson <monson.jeremy@gmail.com>
2018-01-02 13:20:09 -0800

    day 10 completed, need to refactor use iteration since recursion overflows the stack :(

--- day10/day10.js
+++ day10/day10.js
@@ -1,66 +1,58 @@
 /* eslint-disable no-extend-native */
 /* eslint-disable func-names */
 
-function chunkArray(array, groupsize) {
-  const sets = []
-  let i = 0
-  const chunks = array.length / groupsize
+export function chunkArray(array, groupsize) {
+  const sets = [];
+  let i = 0;
+  const chunks = array.length / groupsize;
 
   while (i < chunks) {
-    sets[i] = array.splice(0, groupsize)
-    i += 1
+    sets[i] = array.splice(0, groupsize);
+    i += 1;
   }
 
-  return sets
-}
-
-function interleaveInArray(arr, value) {
-  return arr.reduce((result, element, index, array) => {
-    result.push(element)
-
-    if (index < array.length - 1) {
-      result.push(value)
-    }
-
-    return result
-  }, [])
+  return sets;
 }
 
 function generateInputList(isTest) {
   if (isTest) {
-    return [0, 1, 2, 3, 4]
+    return [0, 1, 2, 3, 4];
   }
   // Create an array of values from 0 -> 255
-  return [...Array(256).keys()]
+  return [...Array(256).keys()];
 }
 
 export function xorArray(arr) {
   // eslint-disable-next-line no-bitwise
-  return arr.reduce((accum, curr) => accum ^ curr)
+  return arr.reduce((accum, curr) => accum ^ curr);
 }
 
 export function convertInputToAscii(input) {
   if (input.length === 0) {
-    return [17, 31, 73, 47, 23]
+    return [17, 31, 73, 47, 23];
   }
-  input = input.split(',').map(char => char.charCodeAt(0))
 
-  return interleaveInArray(input, 44).concat([17, 31, 73, 47, 23])
+  const newInput = [];
+  for (let i = 0; i < input.length; i += 1) {
+    const ascii = input.charCodeAt(i);
+    newInput.push(ascii);
+  }
+  return newInput.concat([17, 31, 73, 47, 23]);
 }
 
 export function createKnotHash(arr) {
   return arr.reduce((accum, curr) => {
-    if (curr.toString(16).length === 1) return `${accum}0${curr.toString(16)}`
-    return accum + curr.toString(16)
-  }, '')
+    if (curr.toString(16).length === 1) return `${accum}0${curr.toString(16)}`;
+    return accum + curr.toString(16);
+  }, '');
 }
 
 export function reverseSubsection(array, start, length) {
   // store length for efficiency
-  const arrayLen = array.length
+  const arrayLen = array.length;
 
   // The actual overlapping value is the remainder of length / arrayLen
-  length %= arrayLen
+  length %= arrayLen;
 
   if (start + length <= arrayLen) {
     // Easy mode, just reverse the sub array and concatenate
@@ -68,19 +60,19 @@ export function reverseSubsection(array, start, length) {
       ...array.slice(0, start),
       ...array.slice(start, start + length).reverse(),
       ...array.slice(start + length)
-    ]
+    ];
   }
 
   // The amount we wrap
-  const wrapAmount = length - (arrayLen - start)
-  const reversedSubArr = [...array.slice(start), ...array.slice(0, wrapAmount)].reverse()
+  const wrapAmount = length - (arrayLen - start);
+  const reversedSubArr = [...array.slice(start), ...array.slice(0, wrapAmount)].reverse();
 
   // Concatenate it into one array
   return [
     ...reversedSubArr.slice(length - wrapAmount),
     ...array.slice(wrapAmount, start),
     ...reversedSubArr.slice(0, length - wrapAmount)
-  ]
+  ];
 }
 
 function recursiveHandler(
@@ -92,33 +84,54 @@ function recursiveHandler(
   numRounds = 0,
   performRounds = false
 ) {
+  // eslint-disable-next-line
+  return tailRecursion(
+    inputList,
+    inputLengths,
+    currLengthIndex,
+    position,
+    skipSize,
+    numRounds,
+    performRounds
+  );
+}
+
+function tailRecursion(
+  inputList,
+  inputLengths,
+  currLengthIndex,
+  position,
+  skipSize,
+  numRounds = 0,
+  performRounds = false
+) {
   // If not base case then create the new list and pass to the recursive step
 
-  const length = parseInt(inputLengths[currLengthIndex], 10)
-
-  const modifiedList = reverseSubsection(inputList, position, length)
-  const modListLength = modifiedList.length
+  const length = parseInt(inputLengths[currLengthIndex], 10);
+  const modifiedList = reverseSubsection(inputList, position, length);
+  const modListLength = modifiedList.length;
 
   // Increment as given in the instructions
-  position += length + skipSize
+  position += length + skipSize;
 
   // Account for position wrapping
-  if (position > modListLength) {
-    position -= modListLength
+  if (position > modListLength - 1) {
+    position %= modListLength;
   }
-  currLengthIndex += 1
-  skipSize += 1
+  currLengthIndex += 1;
+  skipSize += 1;
 
   // base case
   if (currLengthIndex > inputLengths.length - 1) {
     if (!performRounds) {
-      return modifiedList
+      return modifiedList;
     }
-    if (numRounds <= 64) {
-      numRounds += 1890
-      return recursiveHandler(modifiedList, inputLengths, 0, position, skipSize, numRounds, true)
+
+    if (numRounds < 63) {
+      numRounds += 1;
+      return recursiveHandler(modifiedList, inputLengths, 0, position, skipSize, numRounds, true);
     }
-    return modifiedList
+    return modifiedList;
   }
 
   // (Recursion (Recursion (Recursion (Recursion))))
@@ -130,35 +143,33 @@ function recursiveHandler(
     skipSize,
     numRounds,
     performRounds
-  )
+  );
 }
 
 export function part1(inputLengths, isTest = true) {
   // Handle inputs
-  const inputList = generateInputList(isTest)
-  inputLengths = inputLengths.split(',')
+  const inputList = generateInputList(isTest);
+  inputLengths = inputLengths.split(',');
 
-  const finalList = recursiveHandler(inputList, inputLengths, 0, 0, 0)
+  const finalList = recursiveHandler(inputList, inputLengths, 0, 0, 0);
 
-  return finalList[0] * finalList[1]
+  return finalList[0] * finalList[1];
 }
 
 export function part2(inputLengths, isTest = true) {
   // Handle inputs
-  const inputList = generateInputList(isTest)
+  const inputList = generateInputList(isTest);
 
   // Split the input, turn it into ASCII, then concat the array from instructions
-  inputLengths = convertInputToAscii(inputLengths)
-  // console.log(inputLengths)
+  inputLengths = convertInputToAscii(inputLengths);
 
-  const sparseHash = recursiveHandler(inputList, inputLengths, 0, 0, 0, 0, true)
+  const sparseHash = recursiveHandler(inputList, inputLengths, 0, 0, 0, 0, true);
 
-  const chunkSparseHash = chunkArray(sparseHash, 16)
+  const chunkSparseHash = chunkArray(sparseHash, 16);
 
-  const denseHash = chunkSparseHash.map(chunk => xorArray(chunk))
+  const denseHash = chunkSparseHash.map(chunk => xorArray(chunk));
 
-  const knotHash = createKnotHash(denseHash)
+  const knotHash = createKnotHash(denseHash);
 
-  // console.log(knotHash.length)
-  return knotHash
+  return knotHash;
 }
--- day10/test.js
+++ day10/test.js
@@ -1,125 +1,132 @@
-import { expect } from 'chai'
-import fs from 'fs'
+import { expect } from 'chai';
+import fs from 'fs';
 import {
   part1,
   part2,
   reverseSubsection,
   convertInputToAscii,
   xorArray,
-  createKnotHash
-} from './day10'
+  createKnotHash,
+  chunkArray
+} from './day10';
 
-const FINAL_INPUT = fs.readFileSync('./day10/day10.txt').toString('utf-8')
+const FINAL_INPUT = fs.readFileSync('./day10/day10.txt').toString('utf-8');
 
 describe('Day 10: Knot Hash', () => {
   describe('subarray reverse function', () => {
     it('With an input of ([0, 1, 2, 3, 4], 4, 2) it should return [4, 1, 2, 3, 0]', done => {
-      const inputs = [[0, 1, 2, 3, 4], 4, 2]
-      const expected = [4, 1, 2, 3, 0]
-      const result = reverseSubsection(...inputs)
-      result.forEach((num, index) => expect(num).to.equal(expected[index]))
-      done()
-    })
+      const inputs = [[0, 1, 2, 3, 4], 4, 2];
+      const expected = [4, 1, 2, 3, 0];
+      const result = reverseSubsection(...inputs);
+      result.forEach((num, index) => expect(num).to.equal(expected[index]));
+      done();
+    });
     it('With an input of ([0, 1, 2, 3, 4], 4, 3) it should return [0, 4, 2, 3, 1]', done => {
-      const inputs = [[0, 1, 2, 3, 4], 4, 3]
-      const expected = [0, 4, 2, 3, 1]
-      const result = reverseSubsection(...inputs)
-      result.forEach((num, index) => expect(num).to.equal(expected[index]))
-      done()
-    })
+      const inputs = [[0, 1, 2, 3, 4], 4, 3];
+      const expected = [0, 4, 2, 3, 1];
+      const result = reverseSubsection(...inputs);
+      result.forEach((num, index) => expect(num).to.equal(expected[index]));
+      done();
+    });
     it('With an input of ([0, 1, 2, 3, 4], 3, 4) it should return [4, 3, 2, 1, 0]', done => {
-      const inputs = [[0, 1, 2, 3, 4], 3, 4]
-      const expected = [4, 3, 2, 1, 0]
-      const result = reverseSubsection(...inputs)
-      result.forEach((num, index) => expect(num).to.equal(expected[index]))
-      done()
-    })
+      const inputs = [[0, 1, 2, 3, 4], 3, 4];
+      const expected = [4, 3, 2, 1, 0];
+      const result = reverseSubsection(...inputs);
+      result.forEach((num, index) => expect(num).to.equal(expected[index]));
+      done();
+    });
     it('With an input of ([ 2, 1, 0, 3, 4 ], 3, 4) it should return [ 4, 3, 0, 1, 2 ]', done => {
-      const inputs = [[2, 1, 0, 3, 4], 3, 4]
-      const expected = [4, 3, 0, 1, 2]
-      const result = reverseSubsection(...inputs)
-      result.forEach((num, index) => expect(num).to.equal(expected[index]))
-      done()
-    })
-  })
+      const inputs = [[2, 1, 0, 3, 4], 3, 4];
+      const expected = [4, 3, 0, 1, 2];
+      const result = reverseSubsection(...inputs);
+      result.forEach((num, index) => expect(num).to.equal(expected[index]));
+      done();
+    });
+  });
   describe('Convert to ASCII function', () => {
     it('Given the input "1,2,3" it should return [49,44,50,44,51,17,31,73,47,23]', done => {
-      const input = '1,2,3'
-      const expected = [49, 44, 50, 44, 51, 17, 31, 73, 47, 23]
-      expect(convertInputToAscii(input)).to.eql(expected)
-      done()
-    })
-  })
+      const input = '1,2,3';
+      const expected = [49, 44, 50, 44, 51, 17, 31, 73, 47, 23];
+      expect(convertInputToAscii(input)).to.eql(expected);
+      done();
+    });
+  });
+  describe('Chunk Array function', () => {
+    it('Given the inputs "[1, 2, 3, 4]" and "2" it should return [[1, 2], [3, 4]]', done => {
+      const input = [1, 2, 3, 4];
+      const expected = [[1, 2], [3, 4]];
+      expect(chunkArray(input, 2)).to.eql(expected);
+      done();
+    });
+  });
   describe('XOR Array function', () => {
     it('Given the input "[65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22]" it should return 64', done => {
-      const input = [65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22]
-      const expected = 64
-      expect(xorArray(input)).to.eql(expected)
-      done()
-    })
-  })
+      const input = [65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22];
+      const expected = 64;
+      expect(xorArray(input)).to.eql(expected);
+      done();
+    });
+  });
   describe('Knot Hash function', () => {
     it('Given the input "[64, 7, 255]" it should return "4007ff"', done => {
-      const input = [64, 7, 255]
-      const expected = '4007ff'
-      expect(createKnotHash(input)).to.equal(expected)
-      done()
-    })
-  })
+      const input = [64, 7, 255];
+      const expected = '4007ff';
+      expect(createKnotHash(input)).to.equal(expected);
+      done();
+    });
+  });
   describe('part 1', () => {
     it("on inputs '[0, 1, 2, 3, 4]' and '3, 4, 1, 5' should return 12", done => {
-      const input = '3,4,1,5'
-      const expected = 12
-      expect(part1(input, true)).to.equal(expected)
-      done()
-    })
+      const input = '3,4,1,5';
+      const expected = 12;
+      expect(part1(input, true)).to.equal(expected);
+      done();
+    });
     it('on the final input, should return 1935', done => {
-      const input = FINAL_INPUT
-      const expected = 1935
-      expect(part1(input, false)).to.equal(expected)
-      done()
-    })
-  })
+      const input = FINAL_INPUT;
+      const expected = 1935;
+      expect(part1(input, false)).to.equal(expected);
+      done();
+    });
+  });
   describe('part 2', () => {
     it('on the input "", should return "a2582a3a0e66e6e86e3812dcb672a272"', done => {
-      const input = ''
-      const expected = 'a2582a3a0e66e6e86e3812dcb672a272'
-      expect(part2(input, false)).to.equal(expected)
-      done()
-    })
-  })
+      const input = '';
+      const expected = 'a2582a3a0e66e6e86e3812dcb672a272';
+      expect(part2(input, false)).to.equal(expected);
+      done();
+    });
+  });
   describe('part 2', () => {
     it('on the input "AoC 2017", should return "33efeb34ea91902bb2f59c9920caa6cd"', done => {
-      const input = 'AoC 2017'
-      const expected = '33efeb34ea91902bb2f59c9920caa6cd'
-      expect(part2(input, false)).to.equal(expected)
-      done()
-    })
-  })
+      const input = 'AoC 2017';
+      const expected = '33efeb34ea91902bb2f59c9920caa6cd';
+      expect(part2(input, false)).to.equal(expected);
+      done();
+    });
+  });
   describe('part 2', () => {
     it('on the input "1,2,3", should return "3efbe78a8d82f29979031a4aa0b16a9d"', done => {
-      const input = '1,2,3'
-      const expected = '3efbe78a8d82f29979031a4aa0b16a9d'
-      expect(part2(input, false)).to.equal(expected)
-      done()
-    })
-  })
+      const input = '1,2,3';
+      const expected = '3efbe78a8d82f29979031a4aa0b16a9d';
+      expect(part2(input, false)).to.equal(expected);
+      done();
+    });
+  });
   describe('part 2', () => {
     it('on the input "1,2,4", should return "63960835bcdc130f0b66d7ff4f6a5a8e"', done => {
-      const input = '1,2,4'
-      const expected = '63960835bcdc130f0b66d7ff4f6a5a8e'
-      expect(part2(input, false)).to.equal(expected)
-      done()
-    })
-  })
-  // describe('part 2', () => {
-  //   it('on the final input, should return 1935', done => {
-  //     const input = FINAL_INPUT
-  //     const expected = 0
-  //     expect(part2(input, false)).to.equal(expected)
-  //     done()
-  //   })
-  // })
-})
-
-// 49,44,50,44,51,17,31,73,47,23
+      const input = '1,2,4';
+      const expected = '63960835bcdc130f0b66d7ff4f6a5a8e';
+      expect(part2(input, false)).to.equal(expected);
+      done();
+    });
+  });
+  describe('part 2', () => {
+    it('on the final input, should return 1935', done => {
+      const input = FINAL_INPUT;
+      const expected = 0;
+      expect(part2(input, false)).to.equal(expected);
+      done();
+    });
+  });
+});
